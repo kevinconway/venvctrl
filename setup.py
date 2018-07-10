f019c8edd4661c7a53a662732459adbcf513fe1a
Kevin Conway <kevinjacobconway@gmail.com>
2018-07-10 12:31:21 +0000

    Bump version for pypy shebang rewrites

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst', 'r') as readmefile:
 
 setup(
     name='venvctrl',
-    version='0.2.1',
+    version='0.2.2',
     url='https://github.com/kevinconway/venvctrl',
     description='API for virtual environments.',
     author="Kevin Conway",
