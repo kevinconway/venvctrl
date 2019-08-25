61d7754c71a37a0d586d0617d5f8ce4b552ba876
Kevin Conway <kevinjacobconway@gmail.com>
2019-08-24 23:04:11 -0500

    Add recursive detection of .pth files
    Rewrite paths contained in .pth files to prevent bad sys.path additions
    after relocation.

--- venvctrl/venv/relocate.py
+++ venvctrl/venv/relocate.py
@@ -47,6 +47,34 @@ class RelocateMixin(object):
                     )
                     binfile.shebang = os.linesep.join(shebang)
 
+        # Even though wheel is the official format, there are still several
+        # cases in the wild where eggs are being installed. Eggs come with the
+        # possibility of .pth files. Each .pth file contains the path to where
+        # a module can be found. To handle them we must recurse the entire
+        # venv file tree since they can be either at the root of the
+        # site-packages, bundled within an egg directory, or both.
+        original_path = self.path
+        original_abspath = self.abspath
+        dirs = list(self.dirs)
+        files = list(self.files)
+        while len(dirs) > 0 or len(files) > 0:
+            for file_ in files:
+                if file_.abspath.endswith(".pth"):
+                    content = ""
+                    with open(file_.abspath, "r") as source:
+                        # .pth files are almost always very small. Because of
+                        # this we read the whole file as a convenience.
+                        content = source.read()
+                    # It's not certain whether the .pth will have a relative
+                    # or absolute path so we replace both in order of most to
+                    # least specific.
+                    content = content.replace(original_abspath, destination)
+                    content = content.replace(original_path, destination)
+                    with open(file_.abspath, "w") as source:
+                        source.write(content)
+            next_dir = dirs.pop()
+            files = list(next_dir.files)
+
     def move(self, destination):
         """Reconfigure and move the virtual environment to another path.
 
